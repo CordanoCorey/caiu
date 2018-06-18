f9cc41380efb9a9ef35fdd59c3efd7dd47cb7a91
Corey Gelbaugh <cgelbaugh@caiu.org>
2018-06-18 14:49:37 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'docs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'docs';
+}
