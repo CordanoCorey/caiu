122331c1e516e170c21711492f4bb7b711f6b3c8
Corey Gelbaugh <cgelbaugh@caiu.org>
2018-06-18 21:52:04 -0400

    add library project;

---
+++ projects/library/src/lib/library.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { LibraryComponent } from './library.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [LibraryComponent],
+  exports: [LibraryComponent]
+})
+export class LibraryModule { }
---
+++ projects/library/src/public_api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of library
+ */
+
+export * from './lib/library.service';
+export * from './lib/library.component';
+export * from './lib/library.module';
+export * from './shared/models';
+export * from './shared/utils';
