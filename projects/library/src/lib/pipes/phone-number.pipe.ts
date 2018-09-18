b1ac3bacd3a7692ff99c76ab38b41940de4bad48
cordanocorey <corey@cordanosports.com>
2018-09-17 20:18:42 -0400

    null check in phone number pipe;

--- projects/library/src/lib/pipes/phone-number.pipe.ts
+++ projects/library/src/lib/pipes/phone-number.pipe.ts
@@ -6,8 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PhoneNumberPipe implements PipeTransform {
 
   transform(value: string, args?: any): string {
-    return value.length !== 10 ? value
-      : `(${value[0]}${value[1]}${value[2]}) ${value[3]}${value[4]}${value[5]}-${value[6]}${value[7]}${value[8]}${value[9]}`;
+    return value ? value.length !== 10 ? value
+      : `(${value[0]}${value[1]}${value[2]}) ${value[3]}${value[4]}${value[5]}-${value[6]}${value[7]}${value[8]}${value[9]}`
+      : '';
   }
 
 }
